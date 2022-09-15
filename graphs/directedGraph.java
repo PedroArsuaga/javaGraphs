e049c52a271d7fb80315f8276b49cc0b609a3eee
pedro <arsuagapedro@gmail.com>
2022-09-15 19:11:29 -0300

    Implements addEdge and removeEdge for directedGraph.

--- graphs/directedGraph.java
+++ graphs/directedGraph.java
@@ -11,7 +11,7 @@ public class directedGraph<E extends Number> implements graph<E> {
     private class node implements nodeTemplate
     {
         E elem;
-        ArrayList<node> heads, tails; //Lists for head and tail ends adjacent to vertex
+        ArrayList<node> heads, tails; //Lists for head ("moving towards" node) and tail ("moving away from" node) ends adjacent to vertex
         /* Constructor */
         public node(E e)
         {
@@ -50,25 +50,47 @@ public class directedGraph<E extends Number> implements graph<E> {
             int headNodeCount = del.heads.size();
             int tailNodeCount = del.tails.size();
             for(int j = 0; j < headNodeCount; j++)  
-                del.heads.get(j).heads.remove(del);
+                del.heads.get(j).tails.remove(del);
             for(int j = 0; j < tailNodeCount; j++)
-                del.tails.get(j).tails.remove(del);
+                del.tails.get(j).heads.remove(del);
             del = null;
         };
 
         this.nodeCount--;
     };
  
-
+    /* Adds edge from u to v */
     public void addEdge(E u, E v)
     {
-
+        int i = 0; int j = 0;
+        while( (i < this.nodeCount) && (this.nodes.get(i).elem != u)) 
+            i++;
+        while( (j < this.nodeCount) && (this.nodes.get(j).elem != v)) 
+            j++;
+        node U  = this.nodes.get(i); node V = this.nodes.get(j);
+        U.tails.add(V);
+        V.heads.add(U);
     };
  
-
+    /* Removes edge from u to v, or from v to u, if either exists. */
     public void removeEdge(E u, E v)
     {
-
+        int i = 0; int j = 0;
+        while( (i < this.nodeCount) && (this.nodes.get(i).elem != u)) 
+            i++;
+        while( (j < this.nodeCount) && (this.nodes.get(j).elem != v)) 
+            j++;
+        node U  = this.nodes.get(i); node V = this.nodes.get(j);
+        if(U.heads.contains(V)) //There's an edge from V to U
+        {
+            U.heads.remove(V);
+            V.tails.remove(U);
+        }
+        else if(V.heads.contains(U)) //There's an edge from U to V
+        {
+            U.tails.remove(V);
+            V.tails.remove(U);
+        };
     };
 
     public void bfs(E s)
